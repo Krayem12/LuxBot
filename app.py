b8a78f8bf044274bb6c7b45e47f582832282b6a5
Krayem12 <93749589+Krayem12@users.noreply.github.com>
2025-09-04 11:14:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -72,4 +72,5 @@ def webhook():
         return jsonify({"status": "error", "message": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # يستخدم المنفذ من Render أو 5000 محلياً
+    app.run(host="0.0.0.0", port=port)
